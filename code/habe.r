181ebf7a1894562511984d9e5c7cca6eda5a1a3b
hackstutz <rudi@farys.de>
2014-05-26 00:57:27 +0200

    first analyses with HABE data

---
+++ code/habe.r
@@ -0,0 +1,24 @@
+##############################################################
+### Libraries ################################################
+##############################################################
+
+library(foreign)
+library(ggplot2)
+library(dplyr)
+
+##############################################################
+### Setup Data ###############################################
+##############################################################
+
+setwd("C:/Users/Hackstutz/Dropbox/Git/swiss_inequality_development/")
+habe0911<-read.table("C:/Users/Hackstutz/Dropbox/Ungleichheit/HABE/HABE091011/HABE091011_Standard_130717UOe.txt", header=TRUE)
+habe0608<-read.table("C:/Users/Hackstutz/Dropbox/Ungleichheit/HABE/HABE060708/HABE060708_Standard_130124UOe.txt", header=TRUE)
+names(habe0608)<-ifelse(names(habe0608)==names(habe0911),names(habe0608),names(habe0911))
+habe <- rbind(habe0608,habe0911)
+
+##############################################################
+### Analyses #################################################
+##############################################################
+
+ggplot(habe, aes(x=log(VerfuegbaresEinkommen08)))+geom_histogram()+facet_wrap(~Jahr08)
+
