3a2fc2c716bc237d5c9e921aa8bd069b510696e6
hackstutz <rudi@farys.de>
2014-05-26 02:08:10 +0200

    first HABE analyses

--- code/habe.r
+++ code/habe.r
@@ -16,9 +16,15 @@ habe0608<-read.table("C:/Users/Hackstutz/Dropbox/Ungleichheit/HABE/HABE060708/HA
 names(habe0608)<-ifelse(names(habe0608)==names(habe0911),names(habe0608),names(habe0911))
 habe <- rbind(habe0608,habe0911)
 
+habe <- habe %.%
+	group_by(Jahr08) %.%
+	mutate(percentiles = percent_rank(VerfuegbaresEinkommen08))
+
 ##############################################################
 ### Analyses #################################################
 ##############################################################
 
 ggplot(habe, aes(x=log(VerfuegbaresEinkommen08)))+geom_histogram()+facet_wrap(~Jahr08)
-
+ggplot(habe, aes(y=log(VerfuegbaresEinkommen08),x=percentiles))+geom_line()+facet_wrap(~Jahr08)
+ggplot(habe, aes(x=log(VerfuegbaresEinkommen08),group=Jahr08,color=factor(Jahr08)))+geom_density()
+ggplot(filter(habe,Jahr08%in%c(2006,2008)), aes(x=log(VerfuegbaresEinkommen08),group=Jahr08,color=factor(Jahr08)))+geom_density()
